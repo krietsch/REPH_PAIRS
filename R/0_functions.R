8976880aabc55787e48bd0e3f0bc024efb56a2c9
krietsch <jkrietsch@orn.mpg.de>
2021-02-10 18:57:31 +0100

    added speed filter checking for outlier

--- R/0_functions.R
+++ R/0_functions.R
@@ -2,6 +2,103 @@
 # Functions 
 #==============================================================================================================
 
+#' Create base map (Function adjusted from package auksRuak: https://github.com/krietsch/auksRuak)
+#'
+#' Use open street map data to create a basemap with the extent of the data
+#'
+#' @param DT         Name of the data.table
+#' @param lat        Name of the column with latitude (as.character)
+#' @param lon        Name of the column with longitude (as.character)
+#' @param buffer     Buffer around the data
+#' @param sc_dist    Distance of the scale
+#' @param squared    If true buffer is created around the centre and outline squared
+#' @param projection Projection of the data (default is equal area with centre Barrow)
+#'
+#' @return           bm, a ggplot2 base map
+#' @export
+#'
+#' @import           data.table
+#' @importFrom       sf st_as_sf st_transform st_join st_buffer st_intersects st_geometry st_intersection st_bbox st_as_sfc st_crs
+#' @importFrom       magrittr %>%
+#' @importFrom       ggplot2 ggplot geom_sf coord_sf aes theme element_line element_rect element_blank unit
+#' @importFrom       ggspatial annotation_scale
+#'
+#' @examples
+#' # create table with two points
+#' DT = data.table(name = c('NARL', 'Utqiagvik'),
+#'                 lat  = c(71.320854, 71.290246),
+#'                 lon  = c(-156.648210, -156.788622))
+#'
+#' # change projection
+#' st_transform_DT(DT)
+#'
+#' # create base map
+#' bm = create_bm(DT)
+#' bm
+
+create_bm = function(DT, lat = 'lat', lon = 'lon', buffer = 1000, sc_dist, squared = FALSE, 
+                     projection = paste0('+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0',
+                                         ' +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 ')){
+  
+  if(nrow(DT) > 0) {
+    
+    setnames(DT, c(lat, lon), c('lat', 'lon'))
+    
+    if(squared == TRUE){
+      center_lon = min(DT$lon) + (max(DT$lon) - min(DT$lon))/2
+      center_lat = min(DT$lat) + (max(DT$lat) - min(DT$lat))/2
+      center_point = st_point(x = c(center_lon, center_lat)) %>% st_sfc(crs = projection)
+      rs_extent = center_point %>% st_buffer(buffer) %>% st_bbox(crs = projection) %>% st_as_sfc %>% st_geometry
+      rs_extent = st_transform(rs_extent, crs = st_crs(osm_land))
+      bb = st_bbox(rs_extent) %>% data.table
+      
+    } else {
+      
+      st_d = st_as_sf(DT[!is.na(lon), .(lon, lat)], coords = c('lon','lat'), crs = projection)
+      rs_extent = st_d %>% st_bbox(crs = projection) %>% st_as_sfc %>% st_buffer(buffer) %>% st_bbox(crs = projection) %>% st_as_sfc %>% st_geometry
+      rs_extent = st_transform(rs_extent, crs = st_crs(osm_land))
+      bb = st_bbox(rs_extent) %>% data.table
+    }
+    
+    # crop data
+    land      = st_intersection(osm_land, rs_extent)
+    lakes     = st_intersection(osm_lakes, rs_extent)
+    rivers    = st_intersection(osm_rivers, rs_extent)
+    roads     = st_intersection(osm_roads, rs_extent)
+    buildings = st_intersection(osm_buildings, rs_extent)
+    
+    bm =
+      ggplot() +
+      geom_sf(data = land, fill = 'grey92', colour = 'grey80') +
+      geom_sf(data = lakes[lakes$fclass == 'water', ], fill = 'grey85', colour = 'grey80') +
+      geom_sf(data = roads, color = 'grey60') +
+      geom_sf(data = buildings, fill = 'grey50', color = 'grey50', size = 0.3) +
+      coord_sf(expand = FALSE, xlim = c(bb$.[1], bb$.[3]), ylim = c(bb$.[2], bb$.[4])) +
+      ggspatial::annotation_scale(aes(location = 'bl'), text_cex = 0.8, height = unit(0.2, 'cm'),
+                                  pad_x = unit(0.9, 'cm'), pad_y = unit(0.2, 'cm')) +
+      ggspatial::annotation_north_arrow(aes(location = 'bl'), which_north = "true",
+                                        height = unit(0.6, "cm"), width = unit(0.4, "cm"),
+                                        style = ggspatial::north_arrow_orienteering(text_size = 6, fill = c("black", "black")),
+                                        pad_x = unit(0.2, 'cm'), pad_y = unit(0.25, 'cm')) +
+      theme(panel.grid.major = element_line(colour = "transparent"),
+            panel.grid.minor = element_line(colour = "transparent"),
+            panel.background = element_rect(fill = 'white'), 
+            plot.background = element_rect(fill = "transparent", colour = NA),
+            panel.border = element_rect(fill = NA, colour = "black"),
+            axis.text.x = element_blank(), axis.text.y = element_blank(),
+            axis.ticks.x = element_blank(), axis.ticks.y = element_blank(),
+            axis.title = element_blank(), plot.margin = unit(c(0, 0, -0.2, -0.2), "lines"),
+            legend.background = element_rect(fill = 'transparent'), legend.key = element_blank()
+      )
+    
+    setnames(DT, c('lat', 'lon'), c(lat, lon))
+    
+    bm
+    
+  }
+}
+
+
 
 #' Track characteristics
 #'
@@ -88,3 +185,75 @@ speed_filter <- function(DT, ID, speed, max_speed){
     
   }
 }
+
+
+
+
+distance_filter <- function(DT, ID, distance_btw, max_distance, max_distance_before_after){
+  
+  if(nrow(DT) > 0) {
+    setnames(DT, c(ID, distance_btw), c('IDu', 'distance_btw'))
+    
+    # speed over may
+    DT[, distance_over_threshold := distance_btw > max_distance]
+    
+    # select couts
+    bCounter <- function(x){
+      n = length(x)
+      y = x[-1] != x[-n]
+      i = c(which(y | is.na(y)), n)
+      lengths = diff(c(0L, i))
+      bout_length = rep(lengths, lengths)
+      ids = 1:length(lengths)
+      bout_id = rep(ids, lengths)
+      bout_id 
+    }
+    
+    DT[, bout := bCounter(distance_over_threshold), by = IDu]
+    
+    # select only true outlier
+    DT[, bout_lengths := .N, by = .(IDu, bout)]
+    DT[, seq := seq_len(.N), by = .(IDu, bout)]
+    
+    # distance between point before and after
+    DT[, lon_before := data.table::shift(lon, type = 'lag'), by = IDu]
+    DT[, lat_before := data.table::shift(lat, type = 'lag'), by = IDu]
+    DT[, lon_after  := data.table::shift(lon, type = 'lead'), by = IDu]
+    DT[, lat_after  := data.table::shift(lat, type = 'lead'), by = IDu]
+    DT[, distance_btw_ab := sqrt(sum((c(lon_after, lat_after) - c(lon_before, lat_before))^2)), by = 1:nrow(DT)]
+    
+    DT[, distance_over_threshold_ab := distance_btw_ab < max_distance_before_after]
+    
+    # error
+    DT[, error := seq == 2 & bout_lengths == 2 & distance_over_threshold == TRUE & distance_over_threshold_ab == TRUE]
+    
+    # delete unwanted columns
+    DT[, c('bout','bout_lengths', 'seq', 'distance_over_threshold', 'lon_before', 'lat_before', 
+           'lon_after', 'lat_after', 'distance_over_threshold_ab') := NULL]
+    
+    setnames(DT, c('IDu', 'distance_btw'), c(ID, distance_btw))
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R/2_filter_data_and_predict_path.R
+++ R/2_filter_data_and_predict_path.R
@@ -7,7 +7,7 @@
 # 2. 
 
 # Packages
-sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'anytime', 'viridis', 'auksRuak', 'trip', 'foreach'), 
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'anytime', 'viridis', 'auksRuak', 'trip', 'foreach', 'sf'), 
         require, character.only = TRUE)
 
 # Functions
@@ -59,32 +59,33 @@ quantile(d$speed, probs = c(0.99), na.rm = TRUE)
 speed_filter(d, ID = 'ID_year', speed = 'speed', max_speed = 30)
 d[, .N, error]
 
-# visual check of the errors
-de = d[error == TRUE]
-
-foreach(i = 1:nrow(de)) %do% {
-  
-  # subset
-  ID_ = de[i, ]$ID
-  dt_ = de[i, ]$datetime_
-  ds = d[ID == ID_ & datetime_ > c(dt_ - 3600*2) & datetime_ < c(dt_ + 3600*2)]
-  
-  # plot
-  bm = create_bm(ds)
-  bm + 
-    geom_path(data = ds, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) + 
-    geom_point(data = ds, aes(lon, lat, color = speed), size = 1.5) +
-    scale_color_viridis(direction = -1)
-  
-  ggsave(paste0('./OUTPUTS/FIGURES/error/', ID_, '.png'), plot = last_plot(),  
-         width = 177, height = 177, units = c('mm'), dpi = 'print')
-  
-}
-
-
+# # visual check of the errors
+# de = d[error == TRUE]
+# 
+# foreach(i = 1:nrow(de)) %do% {
+# 
+#   # subset
+#   ID_ = de[i, ]$ID
+#   dt_ = de[i, ]$datetime_
+#   ds = d[ID == ID_ & datetime_ > c(dt_ - 3600*2) & datetime_ < c(dt_ + 3600*2)]
+# 
+#   # plot
+#   bm = create_bm(ds)
+#   bm +
+#     geom_path(data = ds, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) +
+#     geom_point(data = ds[error == FALSE], aes(lon, lat), color = 'dodgerblue4', size = 1.5) +
+#     geom_point(data = ds[error == TRUE], aes(lon, lat), color = 'firebrick2', size = 1.5) 
+# 
+#   ggsave(paste0('./OUTPUTS/FIGURES/error_speed_filter_30/', ID_, '.png'), plot = last_plot(),
+#          width = 177, height = 177, units = c('mm'), dpi = 'print')
+# 
+# }
+# 
 # ggplot(data = d[!is.na(speed)]) +
 #   geom_point(aes(speed, gps_speed, color = error))
 
+# all clear errors
+
 # remove errors
 d = d[error == FALSE]
 
@@ -94,6 +95,33 @@ track_characteristics(d, ID = 'ID_year')
 # assign values above threshold
 speed_filter(d, ID = 'ID_year', speed = 'speed', max_speed = 20)
 d[, .N, error]
+
+# # visual check of the errors
+# de = d[error == TRUE]
+# 
+# foreach(i = 1:nrow(de)) %do% {
+# 
+#   # subset
+#   ID_ = de[i, ]$ID
+#   dt_ = de[i, ]$datetime_
+#   ds = d[ID == ID_ & datetime_ > c(dt_ - 3600*2) & datetime_ < c(dt_ + 3600*2)]
+# 
+#   # plot
+#   bm = create_bm(ds)
+#   bm +
+#     geom_path(data = ds, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) +
+#     geom_point(data = ds[error == FALSE], aes(lon, lat), color = 'dodgerblue4', size = 1.5) +
+#     geom_point(data = ds[error == TRUE], aes(lon, lat), color = 'firebrick2', size = 1.5)
+# 
+#   ggsave(paste0('./OUTPUTS/FIGURES/error_speed_filter_20/', ID_, '.png'), plot = last_plot(),
+#          width = 177, height = 177, units = c('mm'), dpi = 'print')
+# 
+# }
+# 
+# ggplot(data = d[!is.na(speed)]) +
+#   geom_point(aes(speed, gps_speed, color = error))
+
+# all clear errors 
 
 # remove errors
 d = d[error == FALSE]
@@ -107,30 +135,171 @@ d[, .N, error]
 
 # visual check of the errors
 de = d[error == TRUE]
+
+d[, speed20 := speed > 20]
 de = d[speed20 == TRUE]
 
+d[, speed_over_threshold := speed > 20]
+
+# # visual check of the errors
+# de = d[error == TRUE]
+# 
+# foreach(i = 1:nrow(de)) %do% {
+#   
+#   # subset
+#   ID_ = de[i, ]$ID
+#   dt_ = de[i, ]$datetime_
+#   ds = d[ID == ID_ & datetime_ > c(dt_ - 3600*2) & datetime_ < c(dt_ + 3600*2)]
+#   
+#   # plot
+#   bm = create_bm(ds)
+#   bm +
+#     geom_path(data = ds, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) +
+#     geom_point(data = ds[error == FALSE], aes(lon, lat), color = 'dodgerblue4', size = 1.5) +
+#     geom_point(data = ds[error == TRUE], aes(lon, lat), color = 'firebrick2', size = 1.5)
+#   
+#   ggsave(paste0('./OUTPUTS/FIGURES/error/', ID_, '.png'), plot = last_plot(),
+#          width = 177, height = 177, units = c('mm'), dpi = 'print')
+#   
+# }
+# 
+# ggplot(data = d[!is.na(speed)]) +
+#   geom_point(aes(speed, gps_speed, color = error))
+
+# some are real tracks
+
+
+# How to separate errors and and true positions
+distance_filter(d, ID = 'ID_year', distance_btw = 'distance_btw', max_distance = 1500, max_distance_before_after = 100)
+
+d[, speed20 := speed > 10]
+
+
+
+# visual check of the errors
+de = d[error == TRUE]
+
 foreach(i = 1:nrow(de)) %do% {
-  
+
   # subset
   ID_ = de[i, ]$ID
   dt_ = de[i, ]$datetime_
-  sp_ = de[i, ]$speed
   ds = d[ID == ID_ & datetime_ > c(dt_ - 3600*2) & datetime_ < c(dt_ + 3600*2)]
-  
+
   # plot
   bm = create_bm(ds)
-  bm + 
-    geom_path(data = ds, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) + 
-    geom_point(data = ds, aes(lon, lat, color = speed), size = 1.5) +
-    ggtitle(sp_) +
-    scale_color_viridis(direction = -1)
-  
-  ggsave(paste0('./OUTPUTS/FIGURES/error/', ID_, '.png'), plot = last_plot(),  
+  bm +
+    geom_path(data = ds, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) +
+    geom_point(data = ds[error == FALSE], aes(lon, lat), color = 'dodgerblue4', size = 1.5) +
+    geom_point(data = ds[error == TRUE], aes(lon, lat), color = 'firebrick2', size = 1.5)
+
+  ggsave(paste0('./OUTPUTS/FIGURES/error_distance/', ID_, '.png'), plot = last_plot(),
          width = 177, height = 177, units = c('mm'), dpi = 'print')
-  
+
 }
 
+# remove errors
+d = d[error == FALSE]
 
+# calculate track characteristics again without errors
+track_characteristics(d, ID = 'ID_year')
+
+ggplot(data = d[!is.na(speed)]) +
+  geom_point(aes(speed, gps_speed, color = error))
+
+hist(d[speed > 5 & speed < 50]$speed*3.6, breaks = 40)
+
+
+
+# create test data
+d1 = data.table(ID = 'bird1', lat = 1:10, lon = 1:10, 
+                datetime_ = seq('2021-01-21 00:01:00' %>% as.POSIXct, '2021-01-21 02:30:00' %>% as.POSIXct, by = '15 mins'))
+
+d2 = data.table(ID = 'bird2', lat = 2:11, lon = 1:10, 
+                datetime_ = seq('2021-01-21 00:01:00' %>% as.POSIXct, '2021-01-21 02:30:00' %>% as.POSIXct, by = '15 mins'))
+
+d3 = data.table(ID = 'bird3', lat = 2:11, lon = 1:10, 
+                datetime_ = seq('2021-01-21 00:01:00' %>% as.POSIXct, '2021-01-21 02:30:00' %>% as.POSIXct, by = '15 mins'))
+
+# add outlier
+d2[5, lat := lat + 20]
+
+# add movement
+d3[6, lat := lat + 9]
+d3[7:10, lat := lat + 20]
+# d3[8, lat := lat + 10]
+
+d = rbindlist(list(d1, d2, d3))
+
+ggplot(data = d) +
+  geom_point(aes(lon, lat, group = ID, color = ID))
+
+track_characteristics(d, ID = 'ID')
+
+
+# speed filter
+# speed_filter(d, ID = 'ID', speed = 'speed', max_speed = 0.02)
+
+# distance filter
+distance_filter(d, ID = 'ID', distance_btw = 'distance_btw', max_distance = 5, max_distance_before_after = 3)
+
+# d[, lon_before := data.table::shift(lon, type = 'lag'), by = ID]
+# d[, lat_before := data.table::shift(lat, type = 'lag'), by = ID]
+# d[, lon_after  := data.table::shift(lon, type = 'lead'), by = ID]
+# d[, lat_after  := data.table::shift(lat, type = 'lead'), by = ID]
+# d[, distance_btw_ab := sqrt(sum((c(lon_after, lat_after) - c(lon_before, lat_before))^2)), by = 1:nrow(d)]
+# 
+# d[distance_over_threshold_ab := distance_btw_ab > max_distance_before_after]
+
+
+ggplot() +
+  geom_path(data = d, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) +
+  geom_point(data = d[error == FALSE], aes(lon, lat), color = 'dodgerblue4', size = 1.5) +
+  geom_point(data = d[error == TRUE], aes(lon, lat), color = 'firebrick2', size = 1.5)
+
+
+d
+
+
+
+
+
+# distance between point before and after
+d[, lon_1         := data.table::shift(lon, type = 'lag'), by = ID]
+d[, lat_1         := data.table::shift(lat, type = 'lag'), by = ID]
+d[, distance_btw_1 := sqrt(sum((c(lon2, lat2) - c(lon_1, lat_1))^2)) , by = 1:nrow(d)]
+
+d[, distance3_bigger_20 := distance_btw3 > 20]
+
+
+de = d[speed_over_threshold == TRUE]
+
+
+# went this fast
+ID_ = 270170765
+dt_ = anytime('2018-07-14 03:01:25')
+
+ds = d[ID == ID_ & datetime_ > c(dt_ - 3600*2) & datetime_ < c(dt_ + 3600*2)]
+
+
+
+ID_ = 270170073
+dt_ = anytime('2018-06-18 11:56:05')
+
+ds = d[ID == ID_ & datetime_ > c(dt_ - 3600) & datetime_ < c(dt_ + 3600)]
+
+
+ggplot(data = ds) +
+  geom_point(aes(datetime_, speed))
+
+ggplot(data = ds) +
+  geom_point(aes(datetime_, distance_btw))
+
+ds = d[ID == ID_]
+
+
+ggplot(data = ds) +
+  geom_point(aes(datetime_, distance_btw))
 
 
 ggplot(data = d[!is.na(speed)]) +
@@ -167,6 +336,12 @@ ggplot(ds) +
   geom_path(aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) + 
   geom_point(aes(lon, lat, color = speed), size = 1.5) + 
   theme_bw()
+
+
+bm = create_bm(d, buffer = 15000)
+
+bm + 
+  geom_path(data = d, aes(lon, lat, group = ID), size = 0.2, color = 'black', alpha = 0.5) 
 
 
 # check with example
