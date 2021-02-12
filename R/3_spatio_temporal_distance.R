22c36900dd8d9463754887dfefacb5f85fd49833
krietsch <jkrietsch@orn.mpg.de>
2021-02-12 10:02:43 +0100

    started script

---
+++ R/3_spatio_temporal_distance.R
@@ -0,0 +1,81 @@
+#' ---
+#' title: Calculate spatio-temporal distance of points
+#' subtitle: 
+#' author: Johannes Krietsch
+#' output:
+#'    html_document:
+#'      toc: true
+#'      highlight: tango
+#' ---
+
+#==============================================================================================================
+# Calculate spatio-temporal distance of points
+#==============================================================================================================
+
+# Summary
+# 1. Apply speed filter 
+# 2. Apply distance filter 
+# 3. Check altitudes
+
+# Packages
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'anytime', 'viridis', 'auksRuak', 'foreach', 'sf', 'knitr'), 
+        require, character.only = TRUE)
+
+# Functions
+source('./R/0_functions.R')
+
+# Lines to run to create html output
+opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
+# rmarkdown::render('./R/3_spatio_temporal_distance.R', output_dir = './OUTPUTS/R_COMPILED')
+
+# Projection
+PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+
+# Data
+d = fread('./DATA/NANO_TAGS_FILTERED.txt', sep = '\t', header = TRUE) %>% data.table
+
+#--------------------------------------------------------------------------------------------------------------
+#' # Distance matrix
+#--------------------------------------------------------------------------------------------------------------
+
+d[, datetime_ := as.POSIXct(datetime_)]
+d[, datetime_10min := round(datetime_, '10 mins')]
+
+
+d_sf = st_as_sf(d, crs = PROJ, coords = c('lon', 'lat'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
