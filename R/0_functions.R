825081061c4f84a2661a6b612487deb623c21ab5
krietsch <jkrietsch@orn.mpg.de>
2021-02-12 10:02:30 +0100

    added function to round datetimes

--- R/0_functions.R
+++ R/0_functions.R
@@ -238,11 +238,38 @@ distance_filter <- function(DT, ID, distance_btw, max_distance, max_distance_bef
 
 
 
+#' Round datetimes
+#'
+#' Written by Rui Barradas: https://stat.ethz.ch/pipermail/r-help/2012-June/315336.html
+#'
+#' @param x 
+#' @param units 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' x <- as.POSIXct(Sys.time() + 1:10*60, tz = "UTC")
+#' x
+#' round(x, "10 mins")
 
-
-
-
-
+round.POSIXct <- function(x, units = c("mins", "5 mins", "10 mins", "15 mins", "quarter hours", "30 mins", "half hours", "hours")){
+  if(is.numeric(units)) units <- as.character(units)
+  units <- match.arg(units)
+  r <- switch(units,
+              "mins" = 60,
+              "5 mins" = 60*5,
+              "10 mins" = 60*10,
+              "15 mins"=, "quarter hours" = 60*15,
+              "30 mins"=, "half hours" = 60*30,
+              "hours" = 60*60)
+  H <- as.integer(format(x, "%H"))
+  M <- as.integer(format(x, "%M"))
+  S <- as.integer(format(x, "%S"))
+  D <- format(x, "%Y-%m-%d")
+  secs <- 3600*H + 60*M + S
+  as.POSIXct(round(secs/r)*r, origin=D, tz = "UTC")
+}
 
 
 
