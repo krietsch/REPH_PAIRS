b5821cd8e8328bf163474a723dfdd616581ce219
krietsch <jkrietsch@orn.mpg.de>
2021-02-15 19:06:24 +0100

    animation of the whole season

--- R/x_animation.R
+++ R/x_animation.R
@@ -15,7 +15,8 @@
 # Summary
 
 # Packages
-sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'anytime', 'viridis', 'auksRuak', 'foreach', 'sf', 'knitr', 'stringr'), 
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'anytime', 'viridis', 'auksRuak', 'foreach', 'sf', 'knitr', 
+          'stringr', 'windR'), 
         require, character.only = TRUE)
 
 # Functions
@@ -62,21 +63,23 @@ bm
 
 
 # plot all
-bm + 
-  geom_path(data = d, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) + 
-  geom_point(data = d, aes(lon, lat, color = sex), size = 0.5) +
-  scale_color_manual(values = c('firebrick2', 'dodgerblue3'))
+# bm + 
+#   geom_path(data = d, aes(lon, lat, group = ID), size = 0.5, color = 'grey', alpha = 0.5) + 
+#   geom_point(data = d, aes(lon, lat, color = sex), size = 0.5) +
+#   scale_color_manual(values = c('firebrick2', 'dodgerblue3'))
 
 
 # Set path to folder where it creates the pictures
 tmp_path = paste0('//ds/grpkempenaers/Hannes/temp/all_reph')
 
 # register cores
-# cl = 50 %>% makePSOCKcluster; registerDoParallel(cl)
+require(doFuture)
+registerDoFuture()
+plan(multiprocess)
 
 # subset time series ----
-ts = data.table( date = seq( d[, (min(datetime_))] %>% as.Date %>% as.POSIXct,
-                             d[, (max(datetime_))] %>% as.Date %>% as.POSIXct, by = '10 mins') )
+ts = data.table( date = seq( d[, (min(datetime_))] %>% as.Date %>% as.POSIXct(., tz = 'UTC'),
+                             d[, (max(datetime_))] %>% as.Date %>% as.POSIXct(., tz = 'UTC'), by = '10 mins') )
 ts = ts[date > d[, (min(datetime_) - 60*60*24)] ]
 ts[, path := paste0(tmp_path, '/', str_pad(1:.N, 4, 'left', pad = '0'), '.png')   ]
 
@@ -85,30 +88,28 @@ ts[, path := paste0(tmp_path, '/', str_pad(1:.N, 4, 'left', pad = '0'), '.png') 
 
 
 foreach(i = 1:nrow(ts), .packages = c('scales', 'ggplot2', 'lubridate', 'stringr', 
-                                      'data.table', 'windR', 'gridGraphics', 'png') ) %dopar% {
+                                      'data.table', 'windR') ) %dopar% {
                                   
   # subset data
   tmp_date = ts[i]$date   # current date
   ds = d[datetime_ <= tmp_date]
-  ds = d[datetime_ %between% c(tmp_date - 60*60*24, tmp_date)]
+  ds = d[datetime_ %between% c(tmp_date - 60*60*6, tmp_date)]
 
   # create alpha and size
   if (nrow(ds) > 0) ds[, a:=  alphaAlong(datetime_, head = 30, skew = -2) ,     by = ID] # alpha
   if (nrow(ds) > 0) ds[, s:=  sizeAlong( datetime_, head = 1, to = c(0.7, 3)) , by = ID] # size
   
- 
-  p = 
-    bm + 
+   bm + 
     
     # track
     geom_path(data = ds, aes(x = lon, y = lat, group = ID), color = 'grey', alpha = ds$a, size = ds$s, lineend = "round") +
 
     # points
-    geom_point(data = ds[datetime_ == min(max(datetime_))], aes(lon, lat, color = sex), size = 0.5) +
+    geom_point(data = ds[ds[, .I[datetime_ == max(datetime_)], by = ID]$V1], aes(lon, lat, color = sex), size = 1, show.legend = FALSE) +
     scale_color_manual(values = c('firebrick2', 'dodgerblue3')) +
     
     # datetime
-    annotate('text', x = c(583430.1 - 350), y = c(7914255 - 120), label = paste0(format(tmp_date, "%B %d %H:00")), size = 12)
+    annotate('text', x = Inf, y = -Inf, hjust = 1.1,  vjust = -0.5, label = paste0(format(tmp_date, "%B %d %H:00")), size = 5)
   
   ggsave(ts[i, path], plot = last_plot(), width = 177, height = 177, units = c('mm'), dpi = 'print')
   
@@ -117,12 +118,9 @@ foreach(i = 1:nrow(ts), .packages = c('scales', 'ggplot2', 'lubridate', 'stringr
                                         
                                         
                                         
-                                        
-# stopCluster(cl)
-# registerDoSEQ()
+               
 
-
-
+# make animation
 wd = getwd()
 setwd(tmp_path)
 system("ffmpeg -framerate 20 -pattern_type glob -i '*.png' -y -c:v libx264 -profile:v high -crf 1 -pix_fmt yuv420p PAIR_NEST.mov")
