2345438dbec5f1971452f2535985efae07c2fbde
krietsch <jkrietsch@orn.mpg.de>
2021-03-25 15:12:24 +0100

    started script to look at distance predictors

---
+++ R/7_distance_predicted_by_movements.R
@@ -0,0 +1,17 @@
+#==============================================================================================================
+# Does the distance between a pair can be explained by the distance before 
+#==============================================================================================================
+
+# Summary
+
+# Packages
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'anytime', 'sf', 'foreach', 'auksRuak', 'knitr', 'foreach',
+          'sdbvis', 'viridis', 'patchwork'),
+        require, character.only = TRUE)
+
+# Functions
+source('./R/0_functions.R')
+
+# Projection
+PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+
