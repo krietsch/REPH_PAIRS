a1ef8fc813664e61d97cd9d82882e2917a4d2255
krietsch <jkrietsch@orn.mpg.de>
2021-03-26 12:00:26 +0100

    loop to read all - found corrupted files

--- R/REPHatBARROW NANO_TAGS check missing data 2021.R
+++ R/REPHatBARROW NANO_TAGS check missing data 2021.R
@@ -13,9 +13,38 @@ f = dbq(con, 'select * FROM NANO_TAGS WHERE FALSE') %>% names
 d = dbq(con, q = 'SELECT * FROM NANO_TAGS') %>% as.data.table
 
 # raw data+
-f18 = list.files('//ds/raw_data_kemp/FIELD/Barrow/2019/DATA/RAW_DATA/NANO_TAG_DATA', pattern = '.csv')
-f19 = list.files('//ds/raw_data_kemp/FIELD/Barrow/2019/DATA/RAW_DATA/NANO_TAG_DATA', pattern = '.csv')
+f18 = list.files('//ds/raw_data_kemp/FIELD/Barrow/2018/DATA/RAW_DATA/NANO_TAG_DATA', pattern = '.csv') %>% data.table
+f18[, year_ := 2018]
+f19 = list.files('//ds/raw_data_kemp/FIELD/Barrow/2019/DATA/RAW_DATA/NANO_TAG_DATA', pattern = '.csv') %>% data.table
+f19[, year_ := 2019]
+fr = rbind(f18, f19)
+setnames(fr, '.', 'file_name')
 
+#-------------------------------------------------------------------------------------------------------------------------
+# Load raw data
+#-------------------------------------------------------------------------------------------------------------------------
+
+# merge all files
+o = foreach(i = 1:nrow(fr), .combine = 'rbind') %do% {
+  
+  x = fread(fr[i, file_path], fill = TRUE)
+  x[, year_ := fr[i, year_]]
+  x[, file_name := fr[i, file_name]]
+  x
+  
+}
+
+
+# file path 
+fr[, file_path := paste0('//ds/raw_data_kemp/FIELD/Barrow/', year_, '/DATA/RAW_DATA/NANO_TAG_DATA/', file_name)]
+
+
+fr[13,]
+
+xt = fread(fr[13, file_path])
+
+
+<<967274;2019;6;13;4;49;33;0;;;;;;;;;;;;;71194002N;156396590W;;0.0;0;4.050;;;;71.3233327+/156.6609833;;1T;2019-06-13T04:49:33.000>>
 #-------------------------------------------------------------------------------------------------------------------------
 # Compare database with raw data
 #-------------------------------------------------------------------------------------------------------------------------
