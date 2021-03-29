469775c4bf86a9c946665f4ac1754a01ffab1e72
krietsch <jkrietsch@orn.mpg.de>
2021-03-29 11:04:19 +0200

    format table to compare with what is in the db

--- R/REPHatBARROW NANO_TAGS check missing data 2021.R
+++ R/REPHatBARROW NANO_TAGS check missing data 2021.R
@@ -11,8 +11,13 @@ sapply( c('data.table', 'sdb','readxl','foreach', 'xlsx', 'wadeR', 'sdbvis', 'sp
 con = dbcon('jkrietsch', db = 'REPHatBARROW')  
 f = dbq(con, 'select * FROM NANO_TAGS WHERE FALSE') %>% names
 d = dbq(con, q = 'SELECT * FROM NANO_TAGS') %>% as.data.table
+cdb = dbq(con, q = 'SELECT * FROM REPHatBARROW.CAPTURES') %>% as.data.table
 
-# raw data+
+drdb18 =  dbq(con, q = 'SELECT * FROM FIELD_2018_REPHatBARROW.NANO_TAGS') %>% as.data.table
+drdb19 =  dbq(con, q = 'SELECT * FROM FIELD_2019_REPHatBARROW.NANO_TAGS') %>% as.data.table
+drdb = rbind(drdb18, drdb19, fill = TRUE) 
+
+# raw data
 f18 = list.files('//ds/raw_data_kemp/FIELD/Barrow/2018/DATA/RAW_DATA/NANO_TAG_DATA', pattern = '.csv') %>% data.table
 f18[, year_ := 2018]
 f19 = list.files('//ds/raw_data_kemp/FIELD/Barrow/2019/DATA/RAW_DATA/NANO_TAG_DATA', pattern = '.csv') %>% data.table
@@ -24,6 +29,9 @@ setnames(fr, '.', 'file_name')
 # Load raw data
 #-------------------------------------------------------------------------------------------------------------------------
 
+# file path 
+fr[, file_path := paste0('//ds/raw_data_kemp/FIELD/Barrow/', year_, '/DATA/RAW_DATA/NANO_TAG_DATA/', file_name)]
+
 # merge all files
 o = foreach(i = 1:nrow(fr), .combine = 'rbind') %do% {
   
@@ -34,62 +42,44 @@ o = foreach(i = 1:nrow(fr), .combine = 'rbind') %do% {
   
 }
 
+# rename table
+dr = data.table(tagID = (as.integer(o[['Device ID']]) - 967000),
+                datetime_ = as.character(anytime(o[, Timestamp], asUTC = TRUE, tz = "UTC")),    
+                lat = o[['Latitude decimal']],   
+                lon = o[['Longitude decimal']],
+                altitude = o[, Altitude],
+                gps_speed = o[, Speed], 
+                batvolt = o[['Battery voltage']],                                   
+                filename = o[, file_name],
+                pk = NA)
 
-# file path 
-fr[, file_path := paste0('//ds/raw_data_kemp/FIELD/Barrow/', year_, '/DATA/RAW_DATA/NANO_TAG_DATA/', file_name)]
-
-
-fr[13,]
-
-xt = fread(fr[13, file_path])
-
-
-<<967274;2019;6;13;4;49;33;0;;;;;;;;;;;;;71194002N;156396590W;;0.0;0;4.050;;;;71.3233327+/156.6609833;;1T;2019-06-13T04:49:33.000>>
 #-------------------------------------------------------------------------------------------------------------------------
 # Compare database with raw data
 #-------------------------------------------------------------------------------------------------------------------------
 
+# change timezone to AKDT
+dr[, datetime_ := anytime(datetime_, asUTC = TRUE, tz = 'America/Anchorage')]
+dr[, datetime_ := anytime(as.character(datetime_))]
+setorder(dr, tagID, datetime_)
 
+# subset REPH
+dr = dr[tagID < 301]
 
+# correct tag with tagID given twice
+dr[tagID == 146 & datetime_ > anytime('2019-06-25 00:00:00'), tagID := 148]
 
-function (dir = "~/ownCloud/RAW_DATA/NANO_TAG_DATA/", db) 
-{
-  if (missing(db)) 
-    db = yy2dbnam(data.table::year(Sys.Date()))
-  allff = data.table(f = list.files(dir, pattern = ".csv"))
-  doneff = idbq(q = "select distinct filename f from NANO_TAGS")[, 
-                                                                 `:=`(done, 1)]
-  newff = merge(allff, doneff, byx = "f", all.x = TRUE)[is.na(done)]
-  con = dbConnect(RMySQL::MySQL(), host = ip(), user = getOption("wader.user"), 
-                  db = db, password = pwd())
-  on.exit(dbDisconnect(con))
-  O = foreach(i = 1:nrow(newff), .combine = rbind, .errorhandling = "remove") %do% 
-    {
-      di = fread(paste(dir, newff[i, f], sep = "/"))
-      setnames(di, make.names(names(di)))
-      di = di[, .(Device.ID, Timestamp, Latitude.decimal, 
-                  Longitude.decimal, Altitude, Speed, Battery.voltage)]
-      di[, `:=`(Timestamp, anytime(Timestamp, asUTC = TRUE, 
-                                   tz = "UTC"))]
-      di[, `:=`(filenam, newff[i, f])]
-      di
-    }
-  if (inherits(O, "data.table")) {
-    O[, `:=`(pk, NA)]
-    O[, `:=`(Device.ID, as.integer(Device.ID) - 967000)]
-    dbnam = idbq(q = "select * from NANO_TAGS where false") %>% 
-      names
-    setnames(O, dbnam)
-    out = dbWriteTable(con, "NANO_TAGS", O, append = TRUE, 
-                       row.names = FALSE)
-    return(out)
-  }
-  else FALSE
-}
+# exclude NA
+dr = dr[!is.na(lon)]
 
+# Information on tag deployment
+cdb[, IDC :=  paste0(UL, '-', UR, '/', LL, '-', LR)]
+cdb[, tag_attached := anytime(released_time)]
 
+c_on = cdb[!is.na(gps_tag),  .(tag_attached, ID, IDC), by = gps_tag]
+dl = dr[, .(last_position = max(datetime_, na.rm = TRUE)), by = tagID]
 
-
+ds = merge(c_on, dl, by.x = 'gps_tag', by.y = 'tagID')
+ds = ds[, .(gps_tag, ID, IDC, tag_attached, last_position, last_on_bird = NA, found = NA)]
 
 
 
