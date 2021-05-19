0e4b031d56115fcab9fd2d5a99cb96e43775eccc
krietsch <jkrietsch@orn.mpg.de>
2021-05-19 15:25:10 +0200

    caluculate pairwise interactions for all individuals

---
+++ R/5_pair_wise_interactions.R
@@ -0,0 +1,39 @@
+#' ---
+#' title: Calculate pair-wise interactions
+#' subtitle: 
+#' author: Johannes Krietsch
+#' output:
+#'    html_document:
+#'      toc: true
+#'      highlight: tango
+#' ---
+
+#==============================================================================================================
+# Calculate spatio-temporal distance of points
+#==============================================================================================================
+
+# Summary
+# 1. Apply speed filter 
+# 2. Apply distance filter 
+# 3. Check altitudes
+
+# Packages
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'auksRuak', 'foreach', 'knitr'), 
+        require, character.only = TRUE)
+
+# Functions
+source('./R/0_functions.R')
+
+# Lines to run to create html output
+opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
+# rmarkdown::render('./R/3_spatio_temporal_distance.R', output_dir = './OUTPUTS/R_COMPILED')
+
+# Projection
+PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+
+# Data
+d = fread('./DATA/NANO_TAGS_FILTERED.txt', sep = '\t', header = TRUE) %>% data.table
+
+con = dbcon('jkrietsch', db = 'REPHatBARROW')  
+dg = dbq(con, 'select * FROM SEX')
+DBI::dbDisconnect(con)
