bad10b6e959bf5bc1c4d6064598b913aa15059e0
krietsch <jkrietsch@orn.mpg.de>
2021-05-27 18:22:17 +0200

    started script to work on space use

---
+++ R/7_space_use_pairs.R
@@ -0,0 +1,42 @@
+#==============================================================================================================
+# Spatial use of pairs 
+#==============================================================================================================
+
+# Summary
+
+# Packages
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'anytime', 'viridis', 'auksRuak', 'foreach', 'sf', 'knitr', 
+          'stringr', 'windR', 'ggnewscale', 'doFuture', 'patchwork'), 
+        require, character.only = TRUE)
+
+# Functions
+source('./R/0_functions.R')
+
+# Lines to run to create html output
+opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
+# rmarkdown::render('./R/3_spatio_temporal_distance.R', output_dir = './OUTPUTS/R_COMPILED')
+
+# Projection
+PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+
+# Data
+d = fread('./DATA/NANO_TAGS_FILTERED.txt', sep = '\t', header = TRUE) %>% data.table
+dp = fread('./DATA/PAIR_WISE_INTERACTIONS.txt', sep = '\t', header = TRUE) %>% data.table
+dp[, year_ := year(datetime_1)]
+
+con = dbcon('jkrietsch', db = 'REPHatBARROW')  
+dg = dbq(con, 'select * FROM SEX')
+dn = dbq(con, 'select * FROM NESTS')
+dn[, nestID := paste0(nest, '_', substr(year_, 3, 4))]
+dn = dn[year_ > 2017]
+dn[, initiation := as.POSIXct(initiation, tz = 'UTC')]
+dn[, initiation_y := as.POSIXct(format(initiation, format = '%m-%d %H:%M:%S'), format = '%m-%d %H:%M:%S', tz = 'UTC')]
+dn[, nest_state_date := as.POSIXct(nest_state_date, tz = 'UTC')]
+DBI::dbDisconnect(con)
+
+# change projection
+st_transform_DT(dn)
+
+#--------------------------------------------------------------------------------------------------------------
+#' # Define breeding pairs with both sexes tagged
+#--------------------------------------------------------------------------------------------------------------
