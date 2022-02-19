8f279205f64fa18442c09c31f07538f7f081a027
krietsch <jkrietsch@orn.mpg.de>
2022-02-19 08:50:47 +0100

    improved plots

--- R/Reply_to_Mihai.R
+++ R/Reply_to_Mihai.R
@@ -22,7 +22,7 @@ Np_min = 0
 # Np_min = 0.5
 
 # plot settings
-margin_ = unit(c(0, 4, 0, 0), 'pt')
+margin_ = unit(c(4, 4, 4, 4), 'pt')
 
 #--------------------------------------------------------------------------------------------------------------
 #' M1 Paired male and female 
@@ -129,7 +129,8 @@ p =
   theme_classic(base_size = 11) +
   theme(legend.position = c(0.9, 0.9), legend.background = element_blank(), plot.margin = margin_) +
   ylab('Proportion of time together') +
-  xlab('Day relative to clutch initiation (= 0)')
+  xlab('Day relative to clutch initiation (= 0)') +
+  ggtitle("Model 1 as in Figure 1B")
 
 p
 
@@ -159,14 +160,6 @@ e = allEffects(fm1, xlevels = 100)$"scale(datetime_rel_pair):initiated" |>
 # predictions are made for the entire range of the data, subset to the relevant interval
 e = e[(initiated == "no" & datetime_rel_pair < 0) | (initiated == "yes" & datetime_rel_pair > 0)]
 
-
-ggplot(e, aes(y = fit, x = datetime_rel_pair, color = initiated, fill = initiated)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.5) +
-  ylab("Probability of interaction") +
-  xlab("Date [0 = nest initiation date]") +
-  ggtitle("Breeding pairs")
-
 p = 
   ggplot() +
   geom_rect(aes(xmin = 0, xmax = 3, ymin = 0, ymax = 1), fill = 'grey90') +
@@ -192,7 +185,8 @@ p =
   theme_classic(base_size = 11) +
   theme(legend.position = c(0.9, 0.9), legend.background = element_blank(), plot.margin = margin_) +
   ylab('Proportion of time together') +
-  xlab('Day relative to clutch initiation (= 0)')
+  xlab('Day relative to clutch initiation (= 0)') +
+  ggtitle("Model 1 split in initiated (yes/no)")
 
 p
 
@@ -262,7 +256,8 @@ p =
   theme_classic(base_size = 11) +
   theme(legend.position = c(0.9, 0.9), legend.background = element_blank(), plot.margin = margin_) +
   ylab('Probability of extra-pair interactions') +
-  xlab('Day relative to clutch initiation (= 0)')
+  xlab('Day relative to clutch initiation (= 0)') +
+  ggtitle("Model 2 & 3 EP interactions as tried in draft")
 
 p
 
@@ -287,13 +282,31 @@ e = allEffects(fm2, xlevels = 100)$"scale(datetime_rel_pair):initiated" |>
 # predictions are made for the entire range of the data, subset to the relevant interval
 e = e[(initiated == "no" & datetime_rel_pair < 0) | (initiated == "yes" & datetime_rel_pair > 0)]
 
+p = 
+  ggplot() +
+  geom_rect(aes(xmin = 0, xmax = 3, ymin = 0, ymax = 1), fill = 'grey90') +
+  scale_color_manual(values = c('darkorange', 'darkgreen'), name = '', 
+                     labels = c('pre initiation', 'post initiation')) +
+  scale_fill_manual(values = c('darkorange', 'darkgreen'), name = '', 
+                    labels = c('pre initiation', 'post initiation')) +
+  geom_line(data = e, aes(y = fit, x = datetime_rel_pair, color = initiated), size = 0.8) +
+  geom_ribbon(data = e, aes(y = fit, x = datetime_rel_pair, ymin = lower, ymax = upper, fill = initiated), 
+              alpha = 0.2) +
+  scale_x_continuous(limits = c(-10.4, 10.4), breaks = seq(-10, 10, 1), 
+                     labels = c('-10', '', '', '', '', '-5', '', '', '', '', '0', 
+                                '', '', '', '', '5', '', '', '', '', '10'),
+                     expand = expansion(add = c(0.2, 0.2))) +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.2), 
+                     labels = c('0.0', '0.2', '0.4', '0.6', '0.8', '1.0'),
+                     expand = expansion(add = c(0, 0))) +
+  theme_classic(base_size = 11) +
+  theme(legend.position = c(0.9, 0.9), legend.background = element_blank(), plot.margin = margin_) +
+  ylab('Probability of extra-pair interactions') +
+  xlab('Day relative to clutch initiation (= 0)') +
+  ggtitle("Model 2 - Males EP interactions - split in initiated (yes/no)")
 
-ggplot(e, aes(y = fit, x = datetime_rel_pair, color = initiated, fill = initiated)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.5) +
-  ylab("Probability of interaction") +
-  xlab("Date [0 = nest initiation date]") +
-  ggtitle("Breeding pairs")
+p
+
 
 # MODEL3 - Fale interacting with other males
 fm3 <- glmmTMB(ID2_any_ep_int ~ scale(datetime_rel_pair) * initiated + year_ +
@@ -313,13 +326,30 @@ e = allEffects(fm3, xlevels = 100)$"scale(datetime_rel_pair):initiated" |>
 # predictions are made for the entire range of the data, subset to the relevant interval
 e = e[(initiated == "no" & datetime_rel_pair < 0) | (initiated == "yes" & datetime_rel_pair > 0)]
 
+p = 
+  ggplot() +
+  geom_rect(aes(xmin = 0, xmax = 3, ymin = 0, ymax = 1), fill = 'grey90') +
+  scale_color_manual(values = c('darkorange', 'darkgreen'), name = '', 
+                     labels = c('pre initiation', 'post initiation')) +
+  scale_fill_manual(values = c('darkorange', 'darkgreen'), name = '', 
+                    labels = c('pre initiation', 'post initiation')) +
+  geom_line(data = e, aes(y = fit, x = datetime_rel_pair, color = initiated), size = 0.8) +
+  geom_ribbon(data = e, aes(y = fit, x = datetime_rel_pair, ymin = lower, ymax = upper, fill = initiated), 
+              alpha = 0.2) +
+  scale_x_continuous(limits = c(-10.4, 10.4), breaks = seq(-10, 10, 1), 
+                     labels = c('-10', '', '', '', '', '-5', '', '', '', '', '0', 
+                                '', '', '', '', '5', '', '', '', '', '10'),
+                     expand = expansion(add = c(0.2, 0.2))) +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.2), 
+                     labels = c('0.0', '0.2', '0.4', '0.6', '0.8', '1.0'),
+                     expand = expansion(add = c(0, 0))) +
+  theme_classic(base_size = 11) +
+  theme(legend.position = c(0.9, 0.9), legend.background = element_blank(), plot.margin = margin_) +
+  ylab('Probability of extra-pair interactions') +
+  xlab('Day relative to clutch initiation (= 0)') +
+  ggtitle("Model 3 - Females EP interactions - split in initiated (yes/no)")
 
-ggplot(e, aes(y = fit, x = datetime_rel_pair, color = initiated, fill = initiated)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.5) +
-  ylab("Probability of interaction") +
-  xlab("Date [0 = nest initiation date]") +
-  ggtitle("Breeding pairs")
+p
 
 
 
