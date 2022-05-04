4909fce1b87a7b999952834f8b7a3598fb0f70ef
krietsch <jkrietsch@orn.mpg.de>
2022-05-04 14:37:45 +0200

    working on tidy model summary

--- R/statistics_proximity between mates.R
+++ R/statistics_proximity between mates.R
@@ -3,8 +3,9 @@
 #==============================================================================================================
 
 # Packages
-sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'foreach', 'knitr', 
-          'stringr', 'doFuture', 'patchwork', 'activity', 'glmmTMB', 'effects', 'performance'), 
+sapply( c('data.table', 'magrittr', 'sdb', 'ggplot2', 'foreach', 'knitr',  'stringr', 
+          'doFuture', 'patchwork', 'activity', 'glmmTMB', 'effects', 'performance', 'broomExtra',
+          'flextable', 'officer', 'dplyr'), 
         require, character.only = TRUE)
 
 # Lines to run to create html output
@@ -17,7 +18,7 @@ dp  = fread('./DATA/PAIR_WISE_INTERACTIONS_BREEDING_PAIRS.txt', sep = '\t', head
 dr  = fread('./DATA/PAIR_WISE_INTERACTIONS_BREEDING_PAIRS_RANDOM.txt', sep = '\t', header = TRUE, nThread = 20) %>% data.table
 
 # subset data for models
-dm = dp[Np >= Np_min & datetime_rel_pair >= -5 & datetime_rel_pair <= 5]
+dm = dp[datetime_rel_pair >= -5 & datetime_rel_pair <= 5]
 
 # factor year
 dm[, year_ := factor(year_)]
@@ -42,10 +43,41 @@ margin_ = unit(c(4, 4, 4, 4), 'pt')
 #' # Model probability of close proximity for breeding pairs 
 #--------------------------------------------------------------------------------------------------------------
 
-#' ### model selection
+# parameter names 
+pn = fread("parname;                                                          parameter
+            (Intercept);                                                      intercept  
+            scale(datetime_rel_pair);                                         relative day
+            initiated_minus3yes;                                              split (after) 
+            initiated_minus2yes;                                              split (after)  
+            initiated_minus1yes;                                              split (after)  
+            initiated_yes;                                                    split (after)
+            initiated_plus1yes;                                               split (after) 
+            initiated_plus2yes;                                               split (after)  
+            initiated_plus3yes;                                               split (after)
+            scale(sin_time);                                                  sin time
+            scale(cos_time);                                                  cos time
+            scale(datetime_rel_pair):initiated_minus3yes;                     relative day x split (after)                       
+            scale(datetime_rel_pair):initiated_minus2yes;                     relative day x split (after)                            
+            scale(datetime_rel_pair):initiated_minus1yes;                     relative day x split (after)                            
+            scale(datetime_rel_pair):initiated_yes;                           relative day x split (after)                      
+            scale(datetime_rel_pair):initiated_plus1yes;                      relative day x split (after)                           
+            scale(datetime_rel_pair):initiated_plus2yes;                      relative day x split (after)                           
+            scale(datetime_rel_pair):initiated_plus3yes;                      relative day x split (after)                           
+            sd__(Intercept);                                                  random intercept
+            sd__poly(datetime_rel_pair, 2)1;                                  relative day (1st poly)              
+            sd__poly(datetime_rel_pair, 2)2;                                  relative day (2nd poly)              
+            r2marg;                                                           R² marginal
+            r2cond;                                                           R² conditional
+            
+", sep = ';')
+
+
+
+
+#' ### model selection ML for AIC comparison and REML for final table
 
 # three days before clutch initiation
-m_3 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus3 +
+m_3ml <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus3 +
                  scale(sin_time) + scale(cos_time) +
                  (1 + poly(datetime_rel_pair, 2) | pairID),
                family = binomial, data = dm,
@@ -53,89 +85,149 @@ m_3 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus3 +
                control = glmmTMBControl(parallel = 15)
 )
 
+
+m_3 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus3 +
+                 scale(sin_time) + scale(cos_time) +
+                 (1 + poly(datetime_rel_pair, 2) | pairID),
+               family = binomial, data = dm,
+               REML = TRUE,
+               control = glmmTMBControl(parallel = 15)
+)
+
 summary(m_3)
 
-x = r2(m_3) |> data.table() |> t() |> data.table()
-setnames(x, c('R2cond', 'R2marg'))
-x_3 = x[, model := 'm_3']
+# create clean summary table 
+y = tidy(m_3) |> data.table()
+x = r2(m_3) |> data.table() 
+
+setnames(x, c('estimate'))
+x[, estimate := as.numeric(estimate)]
+x[, term :=  c('r2cond', 'r2marg')]
+y = rbindlist(list(y, x), use.names = TRUE, fill = TRUE)
+y[, row_order := rownames(y) |> as.numeric()]
+y = merge(y, pn, by.x = 'term', by.y = 'parname')
+setorder(y, row_order)
+y = y[, .(parameter, estimate, s.e. = std.error, statistic, p = p.value)] # subset relevant
+y %>% mutate_if(is.numeric, ~round(., 3)) # round all numeric columns 
 
 # two days before clutch initiation
-m_2 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus2 +
+m_2ml <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus2 +
                  scale(sin_time) + scale(cos_time) +
                  (1 + poly(datetime_rel_pair, 2) | pairID),
                family = binomial, data = dm,
                REML = FALSE,
+               control = glmmTMBControl(parallel = 15)
+)
+
+
+m_2 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus2 +
+                 scale(sin_time) + scale(cos_time) +
+                 (1 + poly(datetime_rel_pair, 2) | pairID),
+               family = binomial, data = dm,
+               REML = TRUE,
                control = glmmTMBControl(parallel = 15)
 )
 
 summary(m_2)
 
 x = r2(m_2) |> data.table() |> t() |> data.table()
-setnames(x, c('R2cond', 'R2marg'))
+setnames(x, c('r2cond', 'r2marg'))
 x_2 = x[, model := 'm_2']
 
 # one day before clutch initiation
-m_1 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus1 +
+m_1ml <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus1 +
                  scale(sin_time) + scale(cos_time) +
                  (1 + poly(datetime_rel_pair, 2) | pairID),
                family = binomial, data = dm,
                REML = FALSE,
+               control = glmmTMBControl(parallel = 15)
+)
+
+m_1 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus1 +
+                 scale(sin_time) + scale(cos_time) +
+                 (1 + poly(datetime_rel_pair, 2) | pairID),
+               family = binomial, data = dm,
+               REML = TRUE,
                control = glmmTMBControl(parallel = 15)
 )
 
 summary(m_1)
 
 x = r2(m_1) |> data.table() |> t() |> data.table()
-setnames(x, c('R2cond', 'R2marg'))
+setnames(x, c('r2cond', 'r2marg'))
 x_1 = x[, model := 'm_1']
 
 # day with clutch initiation
-m0 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated +
+m0ml <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated +
                 scale(sin_time) + scale(cos_time) +
                 (1 + poly(datetime_rel_pair, 2) | pairID),
               family = binomial, data = dm,
               REML = FALSE,
+              control = glmmTMBControl(parallel = 15)
+)
+
+m0 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated +
+                scale(sin_time) + scale(cos_time) +
+                (1 + poly(datetime_rel_pair, 2) | pairID),
+              family = binomial, data = dm,
+              REML = TRUE,
               control = glmmTMBControl(parallel = 15)
 )
 
 summary(m0)
 
 x = r2(m0) |> data.table() |> t() |> data.table()
-setnames(x, c('R2cond', 'R2marg'))
+setnames(x, c('r2cond', 'r2marg'))
 x0 = x[, model := 'm0']
 
 # one day after clutch initiation
+m1ml <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus1 +
+                scale(sin_time) + scale(cos_time) +
+                (1 + poly(datetime_rel_pair, 2) | pairID),
+              family = binomial, data = dm,
+              REML = FALSE,
+              control = glmmTMBControl(parallel = 15)
+)
+
 m1 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus1 +
                  scale(sin_time) + scale(cos_time) +
                  (1 + poly(datetime_rel_pair, 2) | pairID),
                family = binomial, data = dm,
-               REML = FALSE,
+               REML = TRUE,
                control = glmmTMBControl(parallel = 15)
 )
 
 summary(m1)
 
 x = r2(m1) |> data.table() |> t() |> data.table()
-setnames(x, c('R2cond', 'R2marg'))
+setnames(x, c('r2cond', 'r2marg'))
 x1 = x[, model := 'm1']
 
 # two days after clutch initiation
-m2 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus2 +
+m2ml <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus2 +
                 scale(sin_time) + scale(cos_time) +
                 (1 + poly(datetime_rel_pair, 2) | pairID),
               family = binomial, data = dm,
               REML = FALSE,
+              control = glmmTMBControl(parallel = 15)
+)
+
+m2 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus2 +
+                scale(sin_time) + scale(cos_time) +
+                (1 + poly(datetime_rel_pair, 2) | pairID),
+              family = binomial, data = dm,
+              REML = TRUE,
               control = glmmTMBControl(parallel = 15)
 )
 
 summary(m2)
 
 x = r2(m2) |> data.table() |> t() |> data.table()
-setnames(x, c('R2cond', 'R2marg'))
+setnames(x, c('r2cond', 'r2marg'))
 x2 = x[, model := 'm2']
 
 # three days after clutch initiation
-m3 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus3 +
+m3ml <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus3 +
                 scale(sin_time) + scale(cos_time) +
                 (1 + poly(datetime_rel_pair, 2) | pairID),
               family = binomial, data = dm,
@@ -143,18 +235,32 @@ m3 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus3 +
               control = glmmTMBControl(parallel = 15)
 )
 
+m3 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus3 +
+                scale(sin_time) + scale(cos_time) +
+                (1 + poly(datetime_rel_pair, 2) | pairID),
+              family = binomial, data = dm,
+              REML = TRUE,
+              control = glmmTMBControl(parallel = 15)
+)
+
 summary(m3)
 
 x = r2(m3) |> data.table() |> t() |> data.table()
-setnames(x, c('R2cond', 'R2marg'))
+setnames(x, c('r2cond', 'r2marg'))
 x3 = x[, model := 'm3']
 
 # compare models
-MuMIn::model.sel(m_3, m_2, m_1, m0, m1, m2, m3)
+x = MuMIn::model.sel(m_3ml, m_2ml, m_1ml, m0ml, m1ml, m2ml, m3ml)
+rn = rownames(x)
+x = x |> data.table()
+x[, model := rn]
 
-dr2 = data.table(rbindlist(x_3, x_2, x_1, x0, x1, x2, x3))
+dr2 = data.table(rbindlist(list(x_3, x_2, x_1, x0, x1, x2, x3)))
 
+dx = merge(x, dr2, by = 'model')
 
+dx = dx[, .(model, df, AICc, delta, r2marg, r2cond)]
+dx
 
 
 #' ### refit best fitting model with REML = TRUE
@@ -166,6 +272,7 @@ m_2r <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus2 +
                control = glmmTMBControl(parallel = 15)
 )
 
+summary(m_2r)
 
 
 #' ### plot model with raw data
@@ -215,6 +322,67 @@ p =
   ggtitle("")
 
 p
+
+
+
+
+
+
+
+m1 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_plus1 +
+                scale(sin_time) + scale(cos_time) +
+                (1 + poly(datetime_rel_pair, 2) | pairID),
+              family = binomial, data = dm,
+              REML = FALSE,
+              control = glmmTMBControl(parallel = 15)
+)
+
+summary(m1)
+
+# extract model predictions
+e = allEffects(m1, xlevels = 1000)$"scale(datetime_rel_pair):initiated_plus1" |>
+  data.frame() |>
+  setDT()
+
+# predictions are made for the entire range of the data, subset to the relevant interval
+e = e[(initiated_plus1 == "no" & datetime_rel_pair < 1) | (initiated_plus1 == "yes" & datetime_rel_pair > 1)]
+
+require(ggnewscale)
+
+# plot 
+p = 
+  ggplot() +
+  geom_rect(aes(xmin = 0, xmax = 3, ymin = 0, ymax = 1), fill = 'grey90') +
+  geom_path(data = du, aes(y = int_prop, x = datetime_rel_pair0, group = nestID), alpha = 0.15) +
+  # viridis::scale_color_viridis(direction = -1, name = 'N positions') +
+  
+  # new_scale('color') +
+  scale_color_manual(values = c('darkorange', 'darkgreen'), name = '', 
+                     labels = c('pre peak', 'post peak')) +
+  scale_fill_manual(values = c('darkorange', 'darkgreen'), name = '', 
+                    labels = c('pre peak', 'post peak')) +
+  geom_line(data = e, aes(y = fit, x = datetime_rel_pair, color = initiated_plus1), size = 0.8) +
+  geom_ribbon(data = e, aes(y = fit, x = datetime_rel_pair, ymin = lower, ymax = upper, fill = initiated_plus1), alpha = 0.2) +
+  scale_x_continuous(limits = c(-10.4, 10.4), breaks = seq(-10, 10, 1), 
+                     labels = c('-10', '', '', '', '', '-5', '', '', '', '', '0', 
+                                '', '', '', '', '5', '', '', '', '', '10'),
+                     expand = expansion(add = c(0.2, 0.2))) +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.2), 
+                     labels = c('0.0', '0.2', '0.4', '0.6', '0.8', '1.0'),
+                     expand = expansion(add = c(0, 0))) +
+  theme_classic(base_size = 11) +
+  theme(legend.position = c(0.9, 0.9), legend.background = element_blank(), plot.margin = margin_) +
+  ylab('Proportion of time together') +
+  xlab('Day relative to clutch initiation (= 0)') +
+  ggtitle("")
+
+p
+
+
+
+
+
+
 
 
 
