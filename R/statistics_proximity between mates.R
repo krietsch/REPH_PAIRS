15dc94cf8f942c89b58d8af0c1767dfcbc77f3ea
krietsch <jkrietsch@orn.mpg.de>
2022-05-06 12:17:06 +0200

    updated data available figure

--- R/statistics_proximity between mates.R
+++ R/statistics_proximity between mates.R
@@ -1,5 +1,5 @@
 #==============================================================================================================
-# Proximity between mates in relation to breeding phenology
+# Interactions between mates in relation to breeding phenology
 #==============================================================================================================
 
 # Packages
@@ -59,12 +59,12 @@ dm[, initiated_plus3  := fifelse(datetime_rel_pair < 3, "no", "yes")]
 # plot settings
 margin_ = unit(c(4, 4, 4, 4), 'pt')
 
-# start word file
-ESM = read_docx()
+#--------------------------------------------------------------------------------------------------------------
+#' # Model selection probability of close proximity for breeding pairs 
+#--------------------------------------------------------------------------------------------------------------
 
-#--------------------------------------------------------------------------------------------------------------
-#' # Model probability of close proximity for breeding pairs 
-#--------------------------------------------------------------------------------------------------------------
+# start word file for ESM
+ESM = read_docx()
 
 # parameter names 
 pn = fread("parname;                                                          parameter
@@ -417,7 +417,53 @@ ESM = ESM |> body_add_break(pos = 'after')
 # save word file
 print(ESM, target = "./OUTPUTS/ESM/ESM_REPH_PAIRS.docx")
 
-#' ### plot model with raw data
+
+#--------------------------------------------------------------------------------------------------------------
+#' Data available relative to clutch initiation & proportion together by nestID
+#--------------------------------------------------------------------------------------------------------------
+
+dIDs = unique(dID[datetime_rel_pair0 >= -10 & datetime_rel_pair0 <= 10], 
+              by = c('sex', 'nestID', 'datetime_rel_pair0'))
+dIDs = dIDs[, .N, by = .(sex, datetime_rel_pair0, both_tagged_overlapping)]
+dIDs
+
+# pairwise sample size
+du = unique(dp, by = c('pairID', 'nestID', 'datetime_rel_pair0'))
+dss = unique(du[datetime_rel_pair0 >= -10 & datetime_rel_pair0 <= 10], 
+             by = c('nestID', 'datetime_rel_pair0'))
+dss = dss[, .N, by = datetime_rel_pair0]
+dss
+
+# plot data available
+dIDs[, sex := factor(sex, levels = c('F', 'M', 'pair'))]
+
+pa =
+  ggplot(data = dIDs[both_tagged_overlapping == TRUE]) +
+  geom_text(data = dss, aes(datetime_rel_pair0, Inf, label = N), vjust = 1, size = 3) +
+  geom_col(aes(datetime_rel_pair0, N, fill = sex), position=position_dodge(), width = 0.8) +
+  geom_col(data = dss, aes(datetime_rel_pair0, N), fill = 'grey75', width = 0.8) +
+  scale_fill_manual(values = c('firebrick3', 'dodgerblue4', 'grey75'), name = '', 
+                    labels = c('Female only', 'Male only', 'Both'), drop = FALSE) +
+  scale_x_continuous(limits = c(-10.4, 10.4), breaks = seq(-10, 10, 1), 
+                     labels = c('-10', '', '', '', '', '-5', '', '', '', '', '0', 
+                                '', '', '', '', '5', '', '', '', '', '10'),
+                     expand = expansion(add = c(0.2, 0.2))) +
+  scale_y_continuous(expand = expansion(add = c(0, 10))) +
+  theme_classic(base_size = 11) +
+  theme(legend.position = c(0.09, 0.7), legend.background = element_blank(), plot.margin = margin_, 
+        legend.key.width = unit(0.4, 'cm'), legend.key.height = unit(0.4, 'cm')) +
+  ylab('N') +
+  xlab('')
+
+pa
+
+# how many in average
+ds = du[datetime_rel_pair0 >= -5 & datetime_rel_pair0 <= 5]
+ds[, .N, by = datetime_rel_pair0]
+
+ds[, min(Np), by = datetime_rel_pair0]
+ds[, max(Np), by = datetime_rel_pair0]
+ds[, median(Np), by = datetime_rel_pair0]
 
 # Proportion of time together breeders
 dps = dp[interaction == TRUE, .(N_int = .N), by = .(pairID, nestID, datetime_rel_pair0)]
@@ -425,6 +471,54 @@ du = unique(dp, by = c('pairID', 'nestID', 'datetime_rel_pair0'))
 du = merge(du, dps, by = c('pairID', 'nestID', 'datetime_rel_pair0'), all.x = TRUE)
 du[is.na(N_int), N_int := 0]
 du[, int_prop := N_int / N]
+
+pb = 
+  ggplot() +
+  geom_rect(aes(xmin = 0, xmax = 3, ymin = 0, ymax = 1), fill = 'grey90') +
+  geom_path(data = du, aes(y = int_prop, x = datetime_rel_pair0, group = nestID, color = Np), size = 2, alpha = 0.15) +
+  viridis::scale_color_viridis(direction = -1, name = 'Np positions') +
+  scale_x_continuous(limits = c(-10.4, 10.4), breaks = seq(-10, 10, 1), 
+                     labels = c('-10', '', '', '', '', '-5', '', '', '', '', '0', 
+                                '', '', '', '', '5', '', '', '', '', '10'),
+                     expand = expansion(add = c(0.2, 0.2))) +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.2), 
+                     labels = c('0.0', '0.2', '0.4', '0.6', '0.8', '1.0'),
+                     expand = expansion(add = c(0, 0))) +
+  theme_classic(base_size = 11) +
+  theme(legend.position = c(0.9, 0.9), legend.background = element_blank(), plot.margin = margin_) +
+  ylab('Proportion of the day together') +
+  xlab('Day relative to clutch initiation (= 0)') +
+  ggtitle("")
+
+pb
+
+# merge plots
+pa + pb + 
+  plot_layout(nrow = 2) +
+  plot_layout(heights = c(1, 4)) +
+  plot_annotation(tag_levels = 'A')
+
+# ggsave('./OUTPUTS/FIGURES/MATE_GUARDING/Figure_Data_available.tiff', plot = last_plot(),  width = 180, height = 240, units = c('mm'), dpi = 'print')
+
+
+#--------------------------------------------------------------------------------------------------------------
+#' # Plot model selected model
+#--------------------------------------------------------------------------------------------------------------
+
+
+# selected model
+m_2 <- glmmTMB(interaction ~ scale(datetime_rel_pair) * initiated_minus2 +
+                 scale(sin_time) + scale(cos_time) +
+                 (1 + poly(datetime_rel_pair, 2) | nestID),
+               family = binomial, data = dm,
+               REML = TRUE,
+               control = glmmTMBControl(parallel = 15)
+)
+
+summary(m_2)
+
+
+
 
 # extract model predictions
 e = allEffects(m_2, xlevels = 1000)$"scale(datetime_rel_pair):initiated_minus2" |>
