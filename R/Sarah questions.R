647eb1e0a3c32f5a4fab70fc3ab2edc6939a92e2
krietsch <jkrietsch@orn.mpg.de>
2022-11-17 16:52:34 +0100

    selected some example birds for sarah

--- R/Sarah questions.R
+++ R/Sarah questions.R
@@ -20,4 +20,49 @@ d[msr == 1, .N, by = .(year_, external)]
 # Did you think that birds adjusted well to the tags after attachment?
 # Could you share your IACUC and permitting info please? Or when you see a draft in the future you can add it then.
 # Were all your birds tagged pre-breeding?
-  
+
+
+
+#--------------------------------------------------------------------------------------------------------------
+# Examples for Sarah
+#--------------------------------------------------------------------------------------------------------------
+
+
+con = dbcon('jkrietsch', db = 'REPHatBARROW')  
+d = dbq(con, 'select * FROM NANO_TAGS')
+d = d[ID != 999] # exclude test data
+d[is.na(lon)] # check that no NA
+d[, datetime_ := as.POSIXct(datetime_, tz = 'UTC')]
+DBI::dbDisconnect(con)
+
+
+
+
+
+
+# R203_19
+d1 = d[ID == 270170736 & year_ == 2019]
+
+# R321_19
+d2 = d[ID == 270170835 & year_ == 2019]
+
+# R221_19
+d3 = d[ID == 270170929 & year_ == 2019]
+
+# R308_19
+d4 = d[ID == 273145031 & year_ == 2019]
+
+# R213_19
+d5 = d[ID == 273145071 & year_ == 2019]
+
+
+d = rbindlist(list(d1, d2, d3, d4, d5))
+
+# save data
+fwrite(d, './DATA/NANO_TAGS_examples_method_paper.txt', quote = TRUE, sep = '\t', row.names = FALSE)
+
+# d = fread('./DATA/NANO_TAGS_examples_method_paper.txt', sep = '\t', header = TRUE) %>% data.table
+
+
+
+
