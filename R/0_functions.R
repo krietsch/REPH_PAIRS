b1e04597685d93476856e51230dbf3153b0ef095
krietsch <jkrietsch@orn.mpg.de>
2023-03-21 08:31:28 +0100

    added function to connect with closest date

--- R/0_functions.R
+++ R/0_functions.R
@@ -273,7 +273,74 @@ round.POSIXct <- function(x, units = c("mins", "5 mins", "10 mins", "15 mins", "
 
 
 
+#' closestDatetime (from windR)
+#'
+#' Function that finds the closest datetime_ within a vector of given datetimes.
+#' E.g. one has a point of a track at a given time and searches for the closest wind data.
+#'
+#' @param datetime_ Given time as POSIXct
+#' @param datetimes Vector of possible datetimes as POSIXct
+#'
+#' @return Closest datetime as POSIXct
+#' @export
+#'
+#' @examples
+#' d1 = as.POSIXct("2014-06-09 21:44:25 CEST")
+#' dt = as.POSIXct( c("2014-06-08 23:00:00 CEST", "2014-06-09 05:00:00 CEST",
+#'                    "2014-06-09 11:00:00 CEST", "2014-06-09 17:00:00 CEST",
+#'                    "2014-06-09 23:00:00 CEST", "2014-06-10 05:00:00 CEST",
+#'                    "2014-06-10 11:00:00 CEST", "2014-06-10 17:00:00 CEST") )
+#' closestDatetime(datetime_ = d1, datetimes = dt)
 
+closestDatetime = function(datetime_, datetimes){
+  
+  cN = which(abs(datetimes - datetime_) == min(abs(datetimes - datetime_)))
+  cD = as.POSIXct(datetimes[cN])
+  
+  cD[1]
+  
+}
+
+
+
+#' Change the projection within a data table (from auksRuak)
+#'
+#' @param DT   Name of the data.table
+#' @param lat  Name of the column with latitude (as.character)
+#' @param lon  Name of the column with longitude (as.character)
+#' @param from Projection of the data (default is longlat)
+#' @param to   Projection that the data should get (default is equal area with centre Barrow)
+#'
+#' @return     Data.table as it was with changed projection
+#' @export
+#'
+#' @import     data.table
+#' @importFrom sf st_as_sf st_transform st_coordinates
+#' @importFrom magrittr %>%
+#'
+#' @examples
+#' require(data.table)
+#'
+#' DT = data.table(name = c('NARL', 'Utqiagvik'),
+#'                 lat  = c(71.320854, 71.290246),
+#'                 lon  = c(-156.648210, -156.788622))
+#'
+#' st_transform_DT(DT)
+
+st_transform_DT <- function(DT, lat = 'lat', lon = 'lon',
+                            from = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',
+                            to = paste0('+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0',
+                                        ' +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 ')){
+  
+  if(nrow(DT) > 0) {
+    setnames(DT, c(lat, lon), c('lat', 'lon'))
+    pp = st_as_sf(DT, coords = c('lon', 'lat'), crs = from)
+    pp = st_transform(pp, crs = to) %>% st_coordinates
+    DT[, ':=' (lon =  pp[, 1], lat =  pp[, 2]) ]
+    setnames(DT, c('lat', 'lon'), c(lat, lon))
+    setattr(DT, 'proj4string', to)
+  }
+}
 
 
 
